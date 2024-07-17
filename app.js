6335848098257856dbceed0c9eb90e4aff2dd8e9
starclee <sabarivijay214@gmail.com>
2024-07-17 22:19:19 +0530

    done

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ con.on("open", () => {
   console.log("connected...");
 });
 
-app.use(express.json())
+app.use(express.json());
 
 const alienRouter = require("./routes/aliens.js");
 app.use("/aliens", alienRouter);
